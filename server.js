df12f512d2d2d98b61b6fc4661782f7ef12d0dad
salahmak <crsalah3@gmail.com>
2020-03-15 15:14:40 +0100

    fixed a bug

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res) => {
     const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json('incorrect form submission');
+    }
     db.select('email', 'hash').from('login')
         .where('email', '=', email)
         .then(data => {
@@ -48,7 +51,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, password, name } = req.body;
-
+    if (!email || !name || !password) {
+        return res.status(400).json('incorrect form submission');
+    }
 
     var hash = bcrypt.hashSync(password);
 
