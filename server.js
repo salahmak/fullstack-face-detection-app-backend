4054b9989ae7319b85bd11c5bc8970a9ab08820d
salahmak <crsalah3@gmail.com>
2020-03-15 11:51:47 +0100

    updated the database

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const bcrypt = require('bcrypt-nodejs')
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'somepw',
-        database: 'big-brain'
+        host: process.env.DATABASE_URL,
+	ssl: true
     }
 })
 
