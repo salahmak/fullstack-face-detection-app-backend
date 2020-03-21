6f7b241e49158f9de6163a445f1d63709f3b74c1
salahmak <crsalah3@gmail.com>
2020-03-21 18:20:30 +0100

    trying to solve some bug

--- controllers/image.js
+++ controllers/image.js
@@ -1,28 +1,27 @@
 const Clarifai = require('clarifai');
 
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
-});
 
+const app = new Clarifai.App({
+  apiKey: '2b4d0077c2604e8da9b723bd8922658d'
+});
 const handleApiCall = (req, res) => {
   app.models
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch(err => res.status(400).json('unable to work with the API'))
 }
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
