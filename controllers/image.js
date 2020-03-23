9cd49ecc7f38129f4c8abe9858ce216a596e5fda
salahmak <crsalah3@gmail.com>
2020-03-23 21:11:10 +0100

    modified the handleImage

--- controllers/image.js
+++ controllers/image.js
@@ -17,9 +17,8 @@ const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
     .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-      res.json(entries[0]);
+    .then(user => {
+      res.json(user[0]);
     })
     .catch(err => res.status(400).json('unable to get entries'))
 }
