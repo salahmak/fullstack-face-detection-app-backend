97ff53ded83b3a3436fd011c108acb65529f94bb
salahmak <crsalah3@gmail.com>
2020-03-23 22:00:53 +0100

    some fix

--- controllers/image.js
+++ controllers/image.js
@@ -17,8 +17,9 @@ const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
     .increment('entries', 1)
-    .then(user => {
-      res.json(user[0]);
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
     })
     .catch(err => res.status(400).json('unable to get entries'))
 }
